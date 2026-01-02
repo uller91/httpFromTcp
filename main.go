e08700133a4ed01788b437981628ebdb27ab007d
uller91 <girnikis@gmail.com>
2026-01-01 20:40:29 -0800

    getLinesChannel function done

--- main.go
+++ main.go
@@ -8,14 +8,45 @@ import (
 	"strings"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	line := ""
+	go func() {
+		defer close(ch)
+		defer f.Close()
+		for ;; {
+			buffer := make([]byte, 8)
+			read, err := f.Read(buffer)
+			if err == io.EOF {
+				ch <- line
+				break
+			}
+			part := string(buffer[:read])
+			if strings.Contains(part, "\n") {
+				parts := strings.Split(part, "\n")
+				line += parts[0]
+				ch <- line
+				line = parts[1]
+			} else {
+				line += part
+			}
+		}
+	}()
+
+	return ch
+}
+
 func main() {
-	fmt.Println("I hope I get the job!")
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
-
+	
+	lines := getLinesChannel(file)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
+	/*
 	line := ""
 	for ;; {
 		buffer := make([]byte, 8)
@@ -33,6 +64,6 @@ func main() {
 		} else {
 			line += part
 		}
-	}
+	} */
 
 }
