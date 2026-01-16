34f5b150393b91b63e79dada1b45f87cebfecf17
uller91 <girnikis@gmail.com>
2026-01-15 16:21:55 -0800

    reading/parsing in pieces to combine into the full request line

--- internal/request/request.go
+++ internal/request/request.go
@@ -3,7 +3,6 @@ package request
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"io"
 	"strings"
 )
@@ -21,14 +20,36 @@ type RequestLine struct {
 
 const crlf = "\r\n"
 
-type RequestState int
+const bufferSize = 8
+
+type RequestState string
 
 const (
-	Initialized RequestState = iota
-	Done
+	Initialized RequestState = "initialized"
+	Done        RequestState = "done"
 )
 
-func parseRequestLine(bytes []byte) (RequestLine, int, error) {
+func (r *Request) parse(data []byte) (int, error) {
+	switch r.RequestState {
+	case Initialized:
+		reqLine, read, err := parseRequestLine(data)
+		if err != nil {
+			return 0, err
+		}
+		if read == 0 {
+			return 0, nil
+		}
+		r.RequestLine = reqLine
+		r.RequestState = Done
+		return read, nil
+	case Done:
+		return 0, errors.New("error: trying to read data in a done state")
+	default:
+		return 0, errors.New("error: unknown state")
+	}
+}
+
+func parseRequestLine(data []byte) (RequestLine, int, error) {
 	if !bytes.Contains(data, []byte(crlf)) {
 		return RequestLine{}, 0, nil
 	}
@@ -36,7 +57,7 @@ func parseRequestLine(bytes []byte) (RequestLine, int, error) {
 	idx := bytes.Index(data, []byte(crlf))
 	line := string(data[:idx])
 	if line == "" {
-		return nil, 0, errors.New("request line is empty")
+		return RequestLine{}, 0, errors.New("request line is empty")
 	}
 
 	parts := strings.Split(line, " ")
@@ -49,15 +70,15 @@ func parseRequestLine(bytes []byte) (RequestLine, int, error) {
 	}
 
 	if !strings.Contains(parts[2], "HTTP") {
-		return RequestLine{}, errors.New("melformed request line: shuffled parts")
+		return RequestLine{}, 0, errors.New("melformed request line: shuffled parts")
 	}
 
 	version := strings.Split(parts[2], "/")
 	if len(version) != 2 {
-		return RequestLine{}, errors.New("malformed http version in therequest line")
+		return RequestLine{}, 0, errors.New("malformed http version in therequest line")
 	}
 	if version[1] != "1.1" {
-		return RequestLine{}, errors.New("improper http version in therequest line")
+		return RequestLine{}, 0, errors.New("improper http version in therequest line")
 	}
 
 	reqLine := RequestLine{
@@ -69,25 +90,36 @@ func parseRequestLine(bytes []byte) (RequestLine, int, error) {
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	bytes, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
+	buf := make([]byte, bufferSize, bufferSize)
+	readToIndex := 0
+	req := Request{RequestState: Initialized}
 
-	/*
-		reqStr := string(bytes)
-		reqStrs := strings.Split(reqStr, "\r\n")
-		fmt.Println(reqStrs[0])
-		if reqStrs[0] == "" {
-			return nil, errors.New("requesti line is empty")
+	for req.RequestState != Done {
+		if readToIndex == cap(buf) {
+			buf_new := make([]byte, cap(buf)*2, cap(buf)*2)
+			_ = copy(buf_new, buf)
+			buf = buf_new
 		}
-	*/
 
-	reqLine, read, err := parseRequestLine(bytes)
-	if err != nil {
-		return nil, err
+		read, err := reader.Read(buf[readToIndex:])
+		if err == io.EOF {
+			req.RequestState = Done
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		readToIndex += read
+
+		read, err = req.parse(buf)
+		if err != nil {
+			return nil, err
+		}
+		buf_new := make([]byte, cap(buf), cap(buf))
+		_ = copy(buf_new, buf[read:])
+		buf = buf_new
+		readToIndex -= read
 	}
-	req := Request{RequestLine: reqLine}
 
 	return &req, nil
 }
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -67,7 +67,8 @@ func TestChunkReSader(t *testing.T) {
 	assert.Equal(t, "GET", r.RequestLine.Method)
 	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
 	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
-}
+} 
+
 
 func TestRequestLineParse(t *testing.T) {
 	// Test: Good GET Request line
