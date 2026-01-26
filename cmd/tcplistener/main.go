74eb41884a7eaabe75e8135cff0b557e5d268053
uller91 <girnikis@gmail.com>
2026-01-25 19:09:17 -0800

    tcplistener updated

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -80,6 +80,8 @@ func main() {
 		for key, value := range request.Headers {
 			fmt.Printf("- %s: %s\n", key, value)
 		}
+		fmt.Println("Body:")
+		fmt.Println(string(request.Body))
 
 		fmt.Println("TCP connection has been closed!")
 	}
